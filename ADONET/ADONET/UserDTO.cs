9d8921e63aed0913647edf6cafa0a8234abb0cc7
Pham Duc Minh <phamducminh1990@gmail.com>
2016-09-22 23:02:15 +0700

    addd

---
+++ ADONET/ADONET/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONET
+{
+    class UserDTO
+    {
+        public string USER_ID { get; set; }
+        public string USER_NAME { get; set; }
+        public byte AGE { get; set; }
+    }
+}
